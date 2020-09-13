be1a6e9c874be505edb2e9d10fbdb558d9d79fd7
CesarCachay <cesarcachay1996@gmail.com>
2020-09-13 18:14:12 -0500

    Add retrieve endpoint for product

--- controllers/products.ts
+++ controllers/products.ts
@@ -48,11 +48,24 @@ const getProducts = ({ response }: { response: any }) => {
 };
 
 // @GET /api/products/:id - retrieve
-const getProduct = ({ response }: { response: any }) => {
-  response.body = {
-    success: true,
-    data: products,
-  };
+const getProduct = (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  const product: Product | undefined = products.find((p) => p.id === params.id);
+
+  if (product) {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      message: "No product available with that id",
+    };
+  }
 };
 
 // @POST /api/products/:id
