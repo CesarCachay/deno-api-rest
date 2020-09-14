3c18ac171589665b8cb943dac9240d831e7329a6
CesarCachay <cesarcachay1996@gmail.com>
2020-09-13 19:47:01 -0500

    Add post product on controller

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,4 +1,5 @@
 import { Product } from "../types.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 let products: Product[] = [
   {
@@ -8,7 +9,8 @@ let products: Product[] = [
       "Prenda tejida con flecos en la parte final que le añaden largo y movimiento.",
     price: 42,
     size: "M",
-    image: "",
+    image:
+      "https://res.cloudinary.com/rql-products/image/upload/v1600040927/cardigan-guinda-tejido_avkk9z.png",
   },
   {
     id: "2",
@@ -17,7 +19,8 @@ let products: Product[] = [
       "Cuenta con bolsillo lateral en el pecho, detalle de franjas blancas en la mangas (largas) y número 87 parte posterior.",
     price: 42,
     size: "M",
-    image: "",
+    image:
+      "https://res.cloudinary.com/rql-products/image/upload/v1600040959/camisa-franela-cuadros_d60i0f.png",
   },
   {
     id: "3",
@@ -26,7 +29,8 @@ let products: Product[] = [
       "Prenda larga azul noche. Cuenta con logo en el pecho y franjas blancas en las mangas. (100% poliéster)",
     price: 50,
     size: "L",
-    image: "",
+    image:
+      "https://res.cloudinary.com/rql-products/image/upload/v1600040971/sudadera-adidas-climalite_jfxxy3.png",
   },
   {
     id: "4",
@@ -35,7 +39,8 @@ let products: Product[] = [
       "Forro de peluche blanco y forro de franela estampada en las mangas. Cuenta con bolsillo interno al lado izquierdo, dos bolsillos laterales y dos en la parte del pecho.",
     price: 85,
     size: "M",
-    image: "",
+    image:
+      "https://res.cloudinary.com/rql-products/image/upload/v1600040980/casaca-jean-oversize_ya0vbk.png",
   },
 ];
 
@@ -51,13 +56,15 @@ const getProducts = ({ response }: { response: any }) => {
 const getProduct = (
   { params, response }: { params: { id: string }; response: any },
 ) => {
-  const product: Product | undefined = products.find((p) => p.id === params.id);
+  const newProduct: Product | undefined = products.find((product) =>
+    product.id === params.id
+  );
 
-  if (product) {
+  if (newProduct) {
     response.status = 200;
     response.body = {
       success: true,
-      data: product,
+      data: newProduct,
     };
   } else {
     response.status = 404;
@@ -68,12 +75,28 @@ const getProduct = (
   }
 };
 
-// @POST /api/products/:id
-const createProduct = ({ response }: { response: any }) => {
-  response.body = {
-    success: true,
-    data: products,
-  };
+// @POST /api/products
+const createProduct = async (
+  { request, response }: { request: any; response: any },
+) => {
+  const body = await request.body();
+
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      message: "No data send to server",
+    };
+  } else {
+    const product: Product = body.value;
+    product.id = v4.generate();
+    products.push(product);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  }
 };
 
 // @PUT /api/products/:id
--- types.ts
+++ types.ts
@@ -4,5 +4,5 @@ export interface Product {
   description: string;
   price: number;
   size: string;
-  image: string;
+  image: string | undefined;
 }
